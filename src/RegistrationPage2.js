7326cd8daa913c6a1294aea9183d4a283316f609
Anastasia Kaloudi <ciaskious@gmail.com>
2018-06-18 16:25:42 +0200

    updated billing address form

--- src/RegistrationPage2.js
+++ src/RegistrationPage2.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
 import { Link, withRouter } from 'react-router-dom'
-
-import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
-import logo from './images/logos/logoEzept.png'
+import { Button, Checkbox, Form, Grid, Header,  Segment } from 'semantic-ui-react'
+import logo from './images/logos/logoEzept.png';
 
 class RegistrationPage2 extends Component {
-
   render () {
     return (
       <div className="App">
         <header className="App-header">
-          {<img src={logo} className="App-logo" alt="logo"/>}
+          {<img src= {logo} className="App-logo" alt="logo" />}
         </header>
         <style>{`
           body > div,
@@ -19,31 +17,34 @@ class RegistrationPage2 extends Component {
             height: 90%;
           }
           `}</style>
-        <Grid textAlign='center' style={{height: '100%'}} verticalAlign='middle'>
-          <Grid.Column style={{maxWidth: 450}}>
-            <Header as='h2' color='teal' textAlign='center'>
-              Please enter your billing address
-            </Header>
-            <Form size='large'>
-              <Segment stacked>
-                <Form.Field>
-                  <label>Address</label>
-                  <Form.Group widths='equal'>
-                    <Form.Input placeholder='Street'/>
-                    <Form.Input placeholder='Number'/>
-                  </Form.Group>
-                  <Form.Group widths='equal'>
-                    <Form.Input placeholder='PLZ'/>
-                    <Form.Input placeholder='City'/>
-                  </Form.Group>
-                </Form.Field>
-                <Link
-                  to={'/register/3'}><Button primary type='submit'>Continue</Button>
-                </Link>
-              </Segment>
-            </Form>
-          </Grid.Column>
-        </Grid>
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' textAlign='center'>
+          Please enter your billing address
+        </Header>
+        <Form size='large'>
+         <Segment stacked>
+          <Form.Field>
+            <label>Address</label>
+            <Form.Group widths='equal'>
+              <Form.Input placeholder='Street'/>
+              <Form.Input placeholder='Number'/>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Input placeholder='PLZ'/>
+              <Form.Input placeholder='City'/>
+            </Form.Group>
+          </Form.Field>
+          <Link
+            to={'/register/1'}><Button primary type='submit' >Back</Button>
+          </Link>
+          <Link
+            to={'/register/3'}><Button primary type='submit' >Next</Button>
+          </Link>
+         </Segment>
+        </Form>
+        </Grid.Column>
+      </Grid>
       </div>
     )
   }
